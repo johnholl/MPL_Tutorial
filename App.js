d7457eef6d6eefb46a9e82c27f4164e24c3e7fd4
John Holler <johnholl@umich.edu>
2021-05-02 19:42:02 -0400

    tutorial 1

--- App.js
+++ App.js
@@ -1,11 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import {fstore} from './firebase';
 
 export default function App() {
+
+  const [msg, setMsg] = useState("");
+
+
+  useEffect(() => {
+    async function fetchData() {
+      const doc = await fstore.collection("tests").doc("Yjo4Bp9FimYa2pnRrQUG").get();
+      setMsg(doc.data().message);
+  }
+  fetchData();
+  }, [])
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hot reloading rocks!</Text>
+      <Text>{msg}</Text>
       <StatusBar style="auto" />
     </View>
   );
---
+++ firebase.js
@@ -0,0 +1,17 @@
+
+import Firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDnlUlC1EaEXKyMRBW1ZWAde58AkOfRCLs",
+  authDomain: "mpl-backend.firebaseapp.com",
+  projectId: "mpl-backend",
+  storageBucket: "mpl-backend.appspot.com",
+  messagingSenderId: "480378682261",
+  appId: "1:480378682261:web:fa2d8149414228976c149c"
+};
+
+console.log("LENGTH", Firebase.apps.length);
+  const app = Firebase.apps.length===0 ? Firebase.initializeApp(firebaseConfig) : Firebase.app();
+  export const storage = app.storage();
+  export const auth = app.auth();
+  export const fstore = app.firestore();
